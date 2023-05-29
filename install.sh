d2940d20be7fcef1307603aa6722b9d9df26c40b
Kevin Alavik <kevin@alavik.se>
2023-05-29 07:55:04 +0200

    Better installing

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+set -e
+
+# Clone the repository
 git clone https://github.com/kevinalavik/cpkg
 cd cpkg
-sudo chmod +xrw src/cpkg.sh
-sudo cp src/cpkg.sh /usr/local/bin/cpkg
+
+# Make cpkg.sh executable
+chmod +x src/cpkg.sh
+
+# Create a directory for user executables if it doesn't exist
+mkdir -p ~/.local/bin
+
+# Copy cpkg.sh to the user's executable directory
+cp src/cpkg.sh ~/.local/bin/cpkg
+
+# Add the user's executable directory to the PATH, if not already present
+if ! echo $PATH | grep -q "~/.local/bin"; then
+  echo "export PATH=\$PATH:~/.local/bin" >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+# Cleanup
 cd ..
-sudo rm -rf cpkg/
+rm -rf cpkg
